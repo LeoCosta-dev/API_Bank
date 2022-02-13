967cf2973e1a0d0e56c98f0f475b21abdabc1ec8
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2022-02-13 14:54:45 -0300

    criado count DAO

--- src/DAO/CountDAO.js
+++ src/DAO/CountDAO.js
@@ -1,3 +1,66 @@
+const conection = require("../Infra/conection");
+
 class CountDAO{
-    
+    static createCount(count){
+        const query = `
+        INSERT INTO 'COUNT' SET ?
+        `
+        conection.query(query, count, (e) => {
+            if(e){
+                return e
+            } else {
+                return count
+            }
+        })
+    }
+    static listCount(){
+        const query = `
+        SELECT * FROM COUNT
+        `
+        conection.query(query, (e, result) => {
+            if(e){
+                return e
+            } else {
+                return result
+            }
+        })
+    }
+    static listCountForId(id){
+        const query = `
+        SELECT * FROM COUNT WHERE COUNT_ID = ${id}
+        `
+        conection.query(query, (e, result) => {
+            if(e){
+                return e
+            } else {
+                return result
+            }
+        })
+    }
+    static alterCount(id, values){
+        const query = `
+        UPDATE 'COUNT' SET ? WHERE COUNT_ID = ?
+        `
+        conection.query(query, [id, values], (e, result) => {
+            if(e){
+                return e
+            } else {
+                return {result, ...values, id}
+            }
+        })
+    }
+    static deleteCount(id){
+        const query = `
+        DELETE FROM 'COUNT' WHERE COUNT_ID = ?
+        `
+        conection.query(query, id, (e, result) => {
+            if(e){
+                return e
+            } else {
+                return {result, id}
+            }
+        })
+    }
 }
+
+module.exports = CountDAO;
--- src/model/Customer.js
+++ src/model/Customer.js
@@ -1,11 +1,11 @@
 const ValidatorCustomer = require("../services/ValidatorCustomer")
 
 class Customer{
-    constructor(cpf, firstName, lastName, conta){
+    constructor(cpf, firstName, lastName, countId){
         this.cpf = cpf, // 11 digitos
         this.firstName = firstName, // minímo 3 caracteres
         this.lastName = lastName, // minimo 3 caracteres
-        this.count = conta // tipo objeto
+        this.countId = countId // tipo objeto
     }
 }
 
