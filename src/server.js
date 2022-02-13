1533ce2ffe3c3b45fb1602d37ec0643c71f93316
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2022-02-13 15:05:46 -0300

    Conexão e inicio de servidor da API configurado

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
-const app = require("./app")
+const app = require("./app");
+const conection = require("./Infra/conection");
 const PORT = process.env.PORT || 3002;
-require("./infra/conection");
 
-
-app.listen(PORT, () => {
-    console.log(`Servido rodadando na porta ${PORT}.`)
+conection.connect((e) => {
+    if (e){
+        console.log(e)
+    } else {
+        console.log("Conectado com sucesso ao banco de dados!")
+    }
+    app.listen(PORT, () => {
+        console.log(`Servido rodadando na porta ${PORT}.`)
+    })
 })
